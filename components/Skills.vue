3d98a5638431d1a9bbf2171eaa59a0c7214b6ddc
solomon.sitotaw <solomonsitotaw9@gmail.com>
2024-07-17 23:40:02 +0300

    ADD skils page

---
+++ components/Skills.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    skills: Array,
+});
+</script>
+<template>
+    <!-- Skills Gained -->
+    <div class="flex gap-4 flex-wrap text-[10px] lg:text-base">
+        <span
+            v-for="skill in skills"
+            class="bg-primary_border lg:px-3 lg:py-2 px-[12px] py-[7.5px] uppercase"
+            >{{ skill }}</span
+        >
+    </div>
+</template>
